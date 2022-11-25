e540bf3ab184a0111aa3b28fdc92ab965cba1819
PRANESH GUPTA <46838529+pranesh6876@users.noreply.github.com>
2022-11-25 18:38:43 +0530

    Add files via upload

---
+++ src/com/game/snl/models/Ladder.java
@@ -0,0 +1,21 @@
+package com.game.snl.models;
+
+public class Ladder {
+
+	private int start;
+	private int end;
+	
+	public Ladder(int start, int end) {
+		this.start = start;
+		this.end = end;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public int getEnd() {
+		return end;
+	}
+	
+}
---
+++ src/com/game/snl/models/Player.java
@@ -0,0 +1,24 @@
+package com.game.snl.models;
+
+import java.util.UUID;
+
+public class Player {
+
+	private String name;
+	private String id;
+	
+	public Player(String name)
+	{
+		this.name = name;
+		this.id = UUID.randomUUID().toString();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getId() {
+		return id;
+	}
+	
+}
